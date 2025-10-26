daf0707f117adfb0bc35f84d2cfcc0de74ba0d4e
0TheLearner0 <zishankhan.00f@gmail.com>
2025-10-26 09:45:29 +0530

    created the Encapsulation in car class

--- EncapCar.java
+++ EncapCar.java
@@ -1,1 +1,25 @@
-
+public class car{
+	private String carName;
+	private String type;
+	private int year;
+	
+	public String getCarName() {return carName;}
+	public String getCarType() {return type;}
+	public int getCarYear() {return year;}
+	
+	public void setCar(String carName, String type, int year)
+	{
+		this.carName=carName;
+		this.type=type;
+		this.year=year;
+	}
+	public static void main(String[] arg)
+	{
+		car carObj= new car();
+		carObj.setCar("Valkyrie","Supercar",2025);
+		System.out.println("Car: "+carObj.getCarName()+" Type: "+carObj.getCarType()+" Year: "+carObj.getCarYear());
+	}
+	
+	
+	
+}
