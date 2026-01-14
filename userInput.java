c6682c18639b1e29e242e319859bba3734aa3445
0TheLearner0 <zishankhan.00f@gmail.com>
2026-01-14 23:58:18 +0530

    Can see now how we can take input from the user by using scanner class.

--- userInput.java
+++ userInput.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class userInput {
 
+//	The java.util.Scanner class is necessary for taking the user inputs.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Scanner sc=new Scanner(System.in);
+	int age; String name,profession;
+	System.out.println("Enter your name? ");
+	name=sc.nextLine();
+	System.out.println("Enter your age? ");
+	age=sc.nextInt();
+	System.out.println("Enter your profession? ");
+	profession=sc.next();
+	System.out.println("My name is "+name+". I am "+age+" years old. And I am an "+profession+".");
 
 	}
 
